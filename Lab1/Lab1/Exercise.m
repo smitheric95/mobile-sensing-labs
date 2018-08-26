dc82f5f283b2c130cfb9657749bcc925fc12b48e
Jake Carlson <jrcarlson@smu.edu>
2018-08-26 12:19:00 -0500

    Add simple class for holding exercise data

---
+++ Lab1/Lab1/Exercise.m
@@ -0,0 +1,17 @@
+//
+//  ExerciseModel.m
+//  Lab1
+//
+//  Created by Jake Carlson on 8/26/18.
+//  Copyright © 2018 Mobile Sensing. All rights reserved.
+//
+
+#import "Exercise.h"
+
+@interface Exercise ()
+
+@end
+
+@implementation Exercise
+
+@end
