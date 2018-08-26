d5616a4a8e0424eb7541ac72efa2f78d9f54cf1a
Eric Smith <eric@smu.edu>
2018-08-26 11:41:32 -0500

    finalized refactoring

---
+++ Lab1/Lab1/Controllers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab1
+//
+//  Created by Eric Smith on 8/25/18.
+//  Copyright © 2018 Mobile Sensing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
