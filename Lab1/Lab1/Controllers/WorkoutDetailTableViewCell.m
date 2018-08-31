812a124b8eec8f890152938979dfee1084e8e019
Eric Smith <eric@smu.edu>
2018-08-30 20:47:22 -0500

    custom workout detail cell

---
+++ Lab1/Lab1/Controllers/WorkoutDetailTableViewCell.m
@@ -0,0 +1,28 @@
+//
+//  WorkoutDetailTableViewCell.m
+//  Lab1
+//
+//  Created by Eric Smith on 8/30/18.
+//  Copyright © 2018 Mobile Sensing. All rights reserved.
+//
+
+#import "WorkoutDetailTableViewCell.h"
+@interface WorkoutDetailTableViewCell ()
+@property (weak, nonatomic) IBOutlet UIView *exerciseTitleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *setsLabel;
+
+@end
+@implementation WorkoutDetailTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
