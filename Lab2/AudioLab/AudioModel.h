e1520543573b248ca0ab37dc31f6d42c65c8195a
Eric Smith <eric@smu.edu>
2018-09-12 16:11:32 -0500

    getBufferSize

---
+++ Lab2/AudioLab/AudioModel.h
@@ -0,0 +1,14 @@
+//
+//  AudioModel.h
+//  AudioLab
+//
+//  Created by Eric Smith on 9/12/18.
+//  Copyright © 2018 Eric Larson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AudioModel : NSObject
++ (AudioModel *)sharedManager;
+-(int)getBufferSize;
+@end
---
+++ Lab2/AudioLab/AudioModel.m
@@ -0,0 +1,70 @@
+//
+//  AudioModel.m
+//  AudioLab
+//
+//  Created by Eric Smith on 9/12/18.
+//  Copyright © 2018 Eric Larson. All rights reserved.
+//
+
+#import "AudioModel.h"
+#import "Novocaine.h"
+#import "CircularBuffer.h"
+#import "FFTHelper.h"
+
+#define BUFFER_SIZE 2048*4
+
+@interface AudioModel ()
+@property (strong, nonatomic) AudioModel *singleton;
+@property (strong, nonatomic) Novocaine *audioManager;
+@property (strong, nonatomic) CircularBuffer *buffer;
+@property (strong, nonatomic) FFTHelper *fftHelper;
+@property (nonatomic) int bufferSize;
+@end
+
+@implementation AudioModel
+@synthesize singleton = _singleton;
+@synthesize bufferSize = _bufferSize;
+
++ (AudioModel *)sharedManager {
+    NSLog(@"Allocating shared model");
+    static AudioModel *sharedWorkoutModel = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedWorkoutModel = [[self alloc] initOnce];
+    });
+    
+    return sharedWorkoutModel;
+}
+
+- (AudioModel *)initOnce {
+    if (!_singleton)
+        _singleton = [[AudioModel alloc] init];
+    
+    return _singleton;
+}
+
+-(Novocaine*)audioManager{
+    if(!_audioManager){
+        _audioManager = [Novocaine audioManager];
+    }
+    return _audioManager;
+}
+
+-(int)getBufferSize {
+    return BUFFER_SIZE;
+}
+
+-(CircularBuffer*)buffer{
+    if(!_buffer){
+        _buffer = [[CircularBuffer alloc]initWithNumChannels:1 andBufferSize:BUFFER_SIZE];
+    }
+    return _buffer;
+}
+-(FFTHelper*)fftHelper{
+    if(!_fftHelper){
+        _fftHelper = [[FFTHelper alloc]initWithFFTSize:BUFFER_SIZE];
+    }
+    
+    return _fftHelper;
+}
+@end
