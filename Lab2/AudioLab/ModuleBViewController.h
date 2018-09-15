cb4f1ba7cc4b20b7015d347b1c432c4a0af0c5cb
Eric Smith <eric@smu.edu>
2018-09-15 16:01:09 -0500

    ModuleB view

---
+++ Lab2/AudioLab/ModuleBViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModuleBViewController.h
+//  AudioLab
+//
+//  Created by Eric Smith on 9/15/18.
+//  Copyright © 2018 Eric Larson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModuleBViewController : UIViewController
+
+@end
