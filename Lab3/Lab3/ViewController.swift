91d074863c0f9154d1a98584801f3b21cbd580a7
Jake Carlson <jrcarlson@smu.edu>
2018-09-22 17:09:32 -0500

    Gets step updates from core motion

--- Lab3/Lab3/ViewController.swift
+++ Lab3/Lab3/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var stepsTodayLabel: UILabel!
+    @IBOutlet weak var stepsYesterdayLabel: UILabel!
+    let motionModel = CoreMotionModel.getSharedModel()
+    var labelUpdateTimer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.stepsTodayLabel.text = String(self.motionModel.getNumStepsToday())
+        self.stepsYesterdayLabel.text = String(self.motionModel.getNumStepsYesterday())
+        self.labelUpdateTimer = Timer.scheduledTimer(withTimeInterval: 0.2, repeats: true) {
+            _ in DispatchQueue.main.async {
+                self.updateStepLabels()
+            }
+        }
     }
-
+    
+    func updateStepLabels() -> Void {
+        self.stepsTodayLabel.text = String(self.motionModel.getNumStepsToday())
+        self.stepsYesterdayLabel.text = String(self.motionModel.getNumStepsYesterday())
+    }
 
 }
 
