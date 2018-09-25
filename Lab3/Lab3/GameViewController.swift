d26466d1f1e45ac699f7ac5910eef862ad749c7b
Eric Smith <eric@smu.edu>
2018-09-25 18:27:55 -0500

    commented out code

--- Lab3/Lab3/GameViewController.swift
+++ Lab3/Lab3/GameViewController.swift
@@ -7,24 +7,26 @@
 //
 
 import UIKit
+import SpriteKit
 
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //setup game scene
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView // the view in storyboard must be an SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    override var prefersStatusBarHidden : Bool {
+        return true
     }
-    */
-
 }
