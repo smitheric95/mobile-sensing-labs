80f4b736f5a133d7a4ad97501e8bc9793279dabf
Eric Smith <eric@smu.edu>
2018-09-25 17:34:28 -0500

    made module b view controller

---
+++ Lab3/Lab3/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  Lab3
+//
+//  Created by Eric Smith on 9/25/18.
+//  Copyright © 2018 Jake Carlson. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
