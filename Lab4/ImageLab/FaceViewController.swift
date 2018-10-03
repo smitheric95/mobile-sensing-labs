be32089662d24d80c93b69d7d2943b5a819e99a7
Eric Smith <eric@smu.edu>
2018-10-03 11:44:28 -0500

    added opencvbridge subclasses

--- Lab4/ImageLab/FaceViewController.swift
+++ Lab4/ImageLab/FaceViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController   {
+class FaceViewController: UIViewController   {
 
     //MARK: Class Properties
     var filters : [CIFilter]! = nil
