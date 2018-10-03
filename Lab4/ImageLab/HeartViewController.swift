37723b4667760db26521e246f7d46c8e984ecfdd
Jake Carlson <jrcarlson@smu.edu>
2018-10-03 18:30:43 -0500

    Some more infrastructure for switching between tabs

--- Lab4/ImageLab/HeartViewController.swift
+++ Lab4/ImageLab/HeartViewController.swift
@@ -7,15 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
 class HeartViewController: UIViewController {
+    
+    var videoManager:VideoAnalgesic! = nil
+    let bridge = OpenCVBridgeHeart()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.videoManager = VideoAnalgesic.sharedInstance
+        self.videoManager.setCameraPosition(position: AVCaptureDevice.Position.back)
+        
+        self.videoManager.setProcessingBlock(newProcessBlock: self.processImage)
+        
+        if !videoManager.isRunning{
+            videoManager.start()
+        }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.videoManager.setCameraPosition(position: AVCaptureDevice.Position.back)
+        self.videoManager.setProcessingBlock(newProcessBlock: self.processImage)
+        if !videoManager.isRunning{
+            videoManager.start()
+        }
+    }
+    
+    //MARK: Process image output
+    func processImage(inputImage:CIImage) -> CIImage{
+        var retImage = inputImage
+        self.bridge.setTransforms(self.videoManager.transform)
+        self.bridge.setImage(retImage,
+                             withBounds: retImage.extent, // the first face bounds
+            andContext: self.videoManager.getCIContext())
+        
+        self.bridge.processImage()
+        retImage = self.bridge.getImageComposite() // get back opencv processed part of the image (overlayed on original)
+        
+        return retImage
+    }
 
     /*
     // MARK: - Navigation
