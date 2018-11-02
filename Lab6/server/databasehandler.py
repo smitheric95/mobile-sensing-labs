99219b9525c123dbfa4b1878ab2bac25f5f86f1d
Jake Carlson <jrcarlson@smu.edu>
2018-11-01 20:00:44 -0500

    Get request returns all images

--- Lab6/server/databasehandler.py
+++ Lab6/server/databasehandler.py
@@ -18,8 +18,11 @@ import uuid
 IMAGE_PATH = 'images/'
 
 class DatabaseHandler(BaseHandler):
+    @tornado.web.asynchronous
+    @tornado.gen.coroutine
     def get(self):
-        self.write("get")
+        status = yield self._get_all_image_names()
+        self.write_json({ "images": status })
 
     @tornado.web.asynchronous
     @tornado.gen.coroutine
@@ -40,10 +43,21 @@ class DatabaseHandler(BaseHandler):
         with open(IMAGE_PATH + cname, 'wb') as i:
             i.write(file_info['body'])
         self.db.images.insert({
-            "class_name": class_name,
-            "file_name": fname
+            'class_name': class_name,
+            'file_name': fname
         })
         raise gen.Return("uploaded " + fname)
+
+    @tornado.gen.coroutine
+    def _get_all_image_names(self):
+        cursor = self.db.images.find()
+        result = []
+        for i in cursor:
+            result.append({
+                'file_name': i['file_name'],
+                'class_name': i['class_name']
+            })
+        raise gen.Return(result)
 
 class TestHandler(BaseHandler):
     def get(self):
