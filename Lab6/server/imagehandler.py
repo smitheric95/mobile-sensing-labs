6b42878c87a7536dd5a7dfd878e9409577a5420f
Jake Carlson <jrcarlson@smu.edu>
2018-11-10 20:52:00 -0600

    [WIP] Uploading image and getting fragments back, currently timing out

--- Lab6/server/imagehandler.py
+++ Lab6/server/imagehandler.py
@@ -30,7 +30,34 @@ with open('classes.txt', 'r') as f:
 def to_class(ohe_arr):
     return classes[np.argmax(ohe_arr)]
 
-class ImageHandler(BaseHandler):
+class SplitImageHandler(BaseHandler):
+    # send images:
+    # https://gist.github.com/mstaflex/c346b02b215e4099d09b
+    @tornado.web.asynchronous
+    @tornado.gen.coroutine
+    def post(self):
+        image_info = self.request.files['image'][0]
+        images = yield self._split_image(image_info)
+        boundary = str(uuid.uuid4())
+        self.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0')
+        self.set_header('Connection', 'close')
+        self.set_header( 'Content-Type', 'multipart/x-mixed-replace;boundary={}'.format(boundary))
+        for i in images:
+            self.write(boundary + '\r\n')
+            self.write("Content-type: image/jpeg\r\n")
+            self.write("Content-length: {}\r\n\r\n".format(len(i)))
+            self.write(str(i))
+
+        self.write("here")
+
+    @tornado.gen.coroutine
+    def _split_image(self, image):
+        print(image.keys())
+        print(image['filename'])
+        sub_images = get_split_images(image['body'])
+        raise gen.Return(sub_images)
+
+class ImageHandler(SplitImageHandler):
     @tornado.web.asynchronous
     @tornado.gen.coroutine
     def post(self):
@@ -41,9 +68,7 @@ class ImageHandler(BaseHandler):
 
     @tornado.gen.coroutine
     def _label_image(self, image):
-        print(image.keys())
-        print(image['filename'])
-        sub_images = get_split_images(image['body'])
+        sub_images = yield self._split_image(image)
         result = ""
         for i in sub_images:
             result += to_class(MODEL.predict(i))
