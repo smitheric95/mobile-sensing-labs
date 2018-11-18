d5c56421c1bacf2f1887ba8d129bed1a4ffb1051
Jake Carlson <jrcarlson@smu.edu>
2018-11-18 09:21:56 -0600

    Checks if a file can be compiled

--- Project/server/codehandler.py
+++ Project/server/codehandler.py
@@ -27,6 +27,9 @@ class CodeHandler(BaseHandler):
         code_id = uuid.uuid4()
         save_status = yield self._save_code(code_id, code)
         lint_status = yield self._lint_code(code_id)
+        can_compile = yield self._is_compileable(code_id)
+        if can_compile:
+            self.write("compiled")
         self.write(save_status + "\n\n" + lint_status)
 
     @tornado.gen.coroutine
@@ -41,6 +44,14 @@ class CodeHandler(BaseHandler):
         file_name = self._code_id_to_file(code_id)
         subprocess.run(['autopep8', '--in-place', '--aggressive', '--aggressive', file_name])
         raise gen.Return("lint")
+
+    @tornado.gen.coroutine
+    def _is_compileable(self, code_id):
+        file_name = self._code_id_to_file(code_id)
+        compile_check = subprocess.run(['python3', '-m', 'py_compile', file_name])
+        if compile_check.returncode == 0:
+            raise gen.Return(True)
+        raise gen.Return(False)
 
     def _code_id_to_file(self, code_id):
         return CODE_DIR + str(code_id) + '.py'
